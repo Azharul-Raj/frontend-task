27db29f782273f49a56af3cc6f6ed125ebcb104a
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-09-13 22:58:16 +0600

    pdf download method added

--- app/download_invoice/page.tsx
+++ app/download_invoice/page.tsx
@@ -1,26 +1,47 @@
 "use client"
-import React from 'react'
+import React, { useRef } from 'react';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 import useStore from '../hooks/useStore';
 
 function DownloadInvoice() {
+  const pdfRef=useRef(null)
   const products = useStore((state) => state.products);
   const subtotal = products.reduce((acc, curr) => Number(acc) + Number(curr.total), 0)
-  console.log(products)
+  const downloadPDF=()=>{
+    const page=pdfRef.current;
+    if(page){
+      html2canvas(page).then((canvas)=>{
+        const imageData=  canvas.toDataURL('image/png');
+        const pdf=new jsPDF('p','mm','a4',true);
+        const pdfWidth=pdf.internal.pageSize.getWidth();
+        const pdfHeight=pdf.internal.pageSize.getHeight();
+        const imgWidth=canvas.width;
+        const imgHeight=canvas.height;
+        const ratio=Math.min(pdfWidth/imgWidth,pdfHeight/imgHeight);
+        const imgX=(pdfWidth-imgWidth*ratio)/2;
+        const imgY=30;
+        pdf.addImage(imageData,'PNG',imgX,imgY,imgWidth*ratio,imgHeight*ratio);
+        pdf.save('Invoice.pdf')
+      })
+
+    }
+  }
   const tva = 2800.00
   return (
     <section className=''>
       <h2 className='text-center text-3xl font-bold pt-4'>Your Invoice</h2>
       {/* First section */}
-      <div className="p-9">
+      <div ref={pdfRef} className="p-2 md:p-9">
         <div className="flex justify-between items-center py-2">
           <div className="">
             <h4 className='text-lg font-bold'>Invoice No <span className='text-gray-500'>#I909112</span></h4>
             <p className='text-sm font-bold'>Date :<span className='text-gray-400'> 07/09/2022</span></p>
           </div>
-          <div className="">
+          <div className="hidden md:block">
             <h2 className='text-4xl font-bold'>Logo</h2>
           </div>
-          <div className="text-gray-400 text-[16px]">
+          <div className="text-gray-400 text-sm md:text-[16px]">
             <p>1474 Avenue Kwame</p>
             <p>NKRUMAH 10 BP 13395</p>
             <p>10 Ouagadougou, Burkina Faso</p>
@@ -35,12 +56,12 @@ function DownloadInvoice() {
           <div className=""></div>
         </div>
         <div className="flex justify-between items-center pb-3">
-          <div className="text-gray-400 text-[16px]">
+          <div className="text-gray-400 text-sm md:text-[16px]">
             <p>John Doe</p>
             <p>Lize Transport Organization</p>
             <p>+1 (226) 50 272383</p>
           </div>
-          <div className="text-gray-400 text-[16px]">
+          <div className="text-gray-400 text-sm md:text-[16px]">
             <p>1474 Avenue Kwame, </p>
             <p>NKRUMAH 10 BP 13395</p>
             <p>10 Ouagadougou, Burkina Faso</p>
@@ -51,7 +72,7 @@ function DownloadInvoice() {
         <div className="">
           <hr />
           <hr />
-          <div className="flex justify-between items-center text-gray-900 font-semibold py-2">
+          <div className="flex justify-between items-center text-gray-900 text-sm md:text-base font-semibold py-2">
             <p>Products:</p>
             <p>Description:</p>
             <p>Reservation:</p>
@@ -62,7 +83,7 @@ function DownloadInvoice() {
           <hr />
           {
             products.map(({ product_type, product, description, total }, i) => (
-              <div key={i} className="flex justify-between items-center py-2">
+              <div key={i} className="flex justify-between items-center text-xs md:text-base py-2">
                 <p>{product_type}</p>
                 <p>{description}</p>
                 <p>t22f</p>
@@ -86,6 +107,9 @@ function DownloadInvoice() {
           <p className='font-bold text-blue-700'>Total : <span className=''>{subtotal + tva}</span></p>
         </div>
         <hr />
+      </div>
+      <div className="text-center pb-2">
+        <button onClick={downloadPDF} className='px-4 py-2 bg-blue-600 text-semibold rounded-full text-white'>Download/Print as PDF</button>
       </div>
     </section>
   )
